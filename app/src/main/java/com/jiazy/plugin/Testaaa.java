2451eda0c09256a0a0516ba23ef6d9e9c74bb0d7
jzy <jiazy.gdbbk.com>
2018-03-09 20:48:30 +0800

    aaa

---
+++ app/src/main/java/com/jiazy/plugin/Testaaa.java
@@ -0,0 +1,18 @@
+package com.jiazy.plugin;
+
+/**
+ * Created by admin on 2018/3/8.
+ */
+
+public abstract class Testaaa {
+    public Testaaa() {
+    }
+
+    public static void aa() {
+
+    }
+
+    public void aaa() {
+
+    }
+}
---
+++ app/src/main/java/com/jiazy/plugin/Testbbb.java
@@ -0,0 +1,14 @@
+package com.jiazy.plugin;
+
+/**
+ * Created by admin on 2018/3/8.
+ */
+
+public class Testbbb extends Testaaa {
+
+    @Override
+    public void aaa() {
+        super.aaa();
+        aa();
+    }
+}
